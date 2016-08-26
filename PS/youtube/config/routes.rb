573aba68ae847fcd25b97f3846d3814748f3de5d
mrteera <inbox@mrteera.com>
2016-08-27 00:35:48 +0700

    Bring back all default routes

--- PS/youtube/config/routes.rb
+++ PS/youtube/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'problem_sets#index'
-
+  get 'problem_sets/index'
+  get 'youtube_trends/index'
   get 'youtube_trends', to: 'youtube_trends#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
