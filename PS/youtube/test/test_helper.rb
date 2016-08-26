173b60feef49de817183becd408a2d2efc9c4ccc
mrteera <inbox@mrteera.com>
2016-08-27 00:43:47 +0700

    Add coveralls

--- PS/youtube/test/test_helper.rb
+++ PS/youtube/test/test_helper.rb
@@ -1,3 +1,14 @@
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'app/channels/application_cable/channel.rb'
+  add_filter 'app/channels/application_cable/connection.rb'
+  add_filter 'app/mailers/application_mailer.rb'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/models/application_record.rb'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
