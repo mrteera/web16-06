818ea9f876023056e7beb80f6e8c5b8f9513ba81
mrteera <inbox@mrteera.com>
2016-08-27 02:41:11 +0700

    Generate minitest configuration into spec_helper.rb

--- PS/youtube/test/test_helper.rb
+++ PS/youtube/test/test_helper.rb
@@ -9,13 +9,20 @@ SimpleCov.start do
   add_filter 'app/models/application_record.rb'
 end
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails"
+
+# To add Capybara feature tests add `gem "minitest-rails-capybara"`
+# to the test group in the Gemfile and uncomment the following:
+# require "minitest/rails/capybara"
+
+# Uncomment for awesome colorful output
+# require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
   # Add more helper methods to be used by all tests here...
 end
