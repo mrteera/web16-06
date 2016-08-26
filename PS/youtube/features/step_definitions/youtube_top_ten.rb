9c5ddc038b73a2ee1be76393b00ee1e944ae04de
mrteera <inbox@mrteera.com>
2016-08-27 00:11:56 +0700

    Fix rubocop for youtube_top_ten step_definitions

--- PS/youtube/features/step_definitions/youtube_top_ten.rb
+++ PS/youtube/features/step_definitions/youtube_top_ten.rb
@@ -2,7 +2,7 @@ Given(/^Tammy visits home page$/) do
   visit '/'
 end
 
-Then(/^Tammy clicks PS(\d+) link$/) do |arg1|
+Then(/^Tammy clicks PS1 link$/) do
   click_link 'PS1'
 end
 
@@ -10,6 +10,6 @@ Then(/^Tammy should see the title of the list$/) do
   expect(page).to have_content('Top Ten Trending Videos On YouTube')
 end
 
-Then(/^Tammy should see all (\d+) titles of the videos$/) do |arg1|
+Then(/^Tammy should see all (\d+) titles of the videos$/) do # |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
