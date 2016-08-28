0dbc06e284e5898f8311f9022176812624e54f40
Reshma Dangol <resma.dangol@gmail.com>
2016-08-28 19:10:33 +0700

    Change route structure

--- PS/youtube/config/routes.rb
+++ PS/youtube/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'youtube_trends', to: 'youtube_trends#index'
   get 'divide-by-zero', to: 'youtube_trends#divide_by_zero'
   get 'about-to-divide-by-zero', to: 'youtube_trends#about_to_divide_by_zero'
+  get 'ps-1', to: 'youtube_trends#ps1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
