6d622a2266138fecf32f4e09d1582ce0f4124254
mrteera <inbox@mrteera.com>
2016-09-18 17:28:14 +0700

    Apply SSH confnig for fabric

--- PS/ps1/fabfile.py
+++ PS/ps1/fabfile.py
@@ -4,6 +4,7 @@ import os
 def staging():
     env.host_string = os.environ['STAGING_HOSTNAME']
     env.user = os.environ['STAGING_USERNAME']
+    env.use_ssh_config = True
 
 def deploy():
     run("ls -l")
