181f65357e79b0a984dd69059c2b0b6e5b9cc381
mrteera <inbox@mrteera.com>
2016-09-18 16:48:05 +0700

    Fix unprotected SSH private key

--- PS/ps1/deploy.sh
+++ PS/ps1/deploy.sh
@@ -2,6 +2,7 @@
 
 cp ../../.travis/proxy ~/.ssh/config
 mv ../../.travis/web16_deploy_key ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 sed -i "s#image: {}#image: $REPO:$COMMIT#g" docker-compose.production.yml
 scp docker-compose.production.yml $STAGING_USERNAME:$STAGING_DEPLOY_PATH
 
