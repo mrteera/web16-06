8c694f5c4e66761efb407aa142ae616d9a6d9ed8
mrteera <inbox@mrteera.com>
2016-09-19 16:41:54 +0700

    Fix rails environment as workaround

--- PS/ps1/compose/passenger/start-dev.sh
+++ PS/ps1/compose/passenger/start-dev.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-rails db:create db:migrate db:seed
-rails s -p 3000 -b 0.0.0.0
+export rvm_bin_path=/usr/local/rvm/bin
+export GEM_HOME=/usr/local/rvm/gems/ruby-2.3.1
+export IRBRC=/usr/local/rvm/rubies/ruby-2.3.1/.irbrc
+export MY_RUBY_HOME=/usr/local/rvm/rubies/ruby-2.3.1
+export rvm_path=/usr/local/rvm
+export rvm_prefix=/usr/local
+export PATH=/usr/local/rvm/gems/ruby-2.3.1/bin:/usr/local/rvm/gems/ruby-2.3.1@global/bin:/usr/local/rvm/rubies/ruby-2.3.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/rvm/bin
+export GEM_PATH=/usr/local/rvm/gems/ruby-2.3.1:/usr/local/rvm/gems/ruby-2.3.1@global
+export RUBY_VERSION=ruby-2.3.1
+
+/usr/local/rvm/gems/ruby-2.3.1/bin/rails db:create db:migrate db:seed
+/usr/local/rvm/gems/ruby-2.3.1/bin/rails rails s -p 3000 -b 0.0.0.0
