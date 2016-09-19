b69d4c2e58aec63391fee2d22b2aab8a54b43bea
mrteera <inbox@mrteera.com>
2016-09-19 16:57:41 +0700

    Add workaround environment variables into entrypoint script

--- PS/ps1/compose/passenger/entrypoint.sh
+++ PS/ps1/compose/passenger/entrypoint.sh
@@ -2,6 +2,16 @@
 set -e
 cmd="$@"
 
+export rvm_bin_path=/usr/local/rvm/bin
+export GEM_HOME=/usr/local/rvm/gems/ruby-2.3.1
+export IRBRC=/usr/local/rvm/rubies/ruby-2.3.1/.irbrc
+export MY_RUBY_HOME=/usr/local/rvm/rubies/ruby-2.3.1
+export rvm_path=/usr/local/rvm
+export rvm_prefix=/usr/local
+export PATH=/usr/local/rvm/gems/ruby-2.3.1/bin:/usr/local/rvm/gems/ruby-2.3.1@global/bin:/usr/local/rvm/rubies/ruby-2.3.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/rvm/bin
+export GEM_PATH=/usr/local/rvm/gems/ruby-2.3.1:/usr/local/rvm/gems/ruby-2.3.1@global
+export RUBY_VERSION=ruby-2.3.1
+
 # the official postgres image uses 'postgres' as default user if not set explictly.
 if [ -z "$POSTGRES_USER" ]; then
     export POSTGRES_USER=postgres
