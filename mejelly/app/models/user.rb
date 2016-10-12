fe9fa7279abe22f057d480c5819fc00863417b39
reshma <resma.dangol@gmail.com>
2016-10-13 02:10:59 +0700

    Set default value for is_admin and is_banned

--- mejelly/app/models/user.rb
+++ mejelly/app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_save :default_value
+
+  def default_value
+    self.is_admin ||= false
+    self.is_banned ||= false
+  end
 
   def active_for_authentication?
     super && !self.is_banned?
